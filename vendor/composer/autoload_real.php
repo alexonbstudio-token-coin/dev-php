39bca096b8b254631ff84c2b392e0fdb192ca598
AlexonbStudio <65239996+alexonbstudio@users.noreply.github.com>
2021-06-27 18:11:33 +0200

    dump-autoload

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -34,22 +34,13 @@ class ComposerAutoloaderInit550815746171ec73240ba9d52f182f2e
 
             call_user_func(\Composer\Autoload\ComposerStaticInit550815746171ec73240ba9d52f182f2e::getInitializer($loader));
         } else {
-            $map = require __DIR__ . '/autoload_namespaces.php';
-            foreach ($map as $namespace => $path) {
-                $loader->set($namespace, $path);
-            }
-
-            $map = require __DIR__ . '/autoload_psr4.php';
-            foreach ($map as $namespace => $path) {
-                $loader->setPsr4($namespace, $path);
-            }
-
             $classMap = require __DIR__ . '/autoload_classmap.php';
             if ($classMap) {
                 $loader->addClassMap($classMap);
             }
         }
 
+        $loader->setClassMapAuthoritative(true);
         $loader->register(true);
 
         if ($useStaticLoader) {
